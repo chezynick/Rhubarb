c929cfcadaa7da53fd134d805caa7195575ffaca
chezynick <chezynickannie@yahoo.co.uk>
2020-10-09 10:32:08 +0100

    updated contact details

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,25 @@
+import { EvalSourceMapDevToolPlugin } from "webpack";
+
 function contactDeets () {
     let contactPage = document.getElementById('content');
     contactPage.innerHTML = '';
     contactPage.classList.add('contactPage');
     const map = document.createElement('img');
     const contacts = document.createElement('div');
+    const deets = document.createElement('div')
+    const phone = document.createElement('div')
+    const email = document.createElement('div')
     map.classList.add('map');
     contacts.classList.add('contactBox')
     map.setAttribute('src','https://api2.ordnancesurvey.co.uk/mapping_api/v1/service/zxy/EPSG%3A3857/Outdoor%203857/12/2025/1325.png?key=n7AcuJE53SEuomCSn1dGpgNz1ZxGwU3C');
-    contacts.innerText = 'the numbers \n go here!';
+    deets.innerText = 'To book a table please \n contact us directly';
+    phone.innerText = 'Phone Number: 01457 8866666';
+    email.innerText = 'Email : blahblahblah@Rhubarb.com'
+    //append details to contacts
+    contacts.appendChild(deets)
+    contacts.appendChild(phone);
+    contacts.appendChild(email);
+    //append both boxes to contact page
     contactPage.appendChild(map);
     contactPage.appendChild(contacts);
 }
